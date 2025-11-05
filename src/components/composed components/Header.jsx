86c113cbb2cc788206344f1e3fb9ff5e4c98ab0f
Krishna Gautam <krishnagautam1631@gmail.com>
2025-11-05 10:34:51 +0530

    Added design analysis and implementation plan

---
+++ src/components/composed components/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <div className="logo">
+                    <h1>lgogo</h1>
+                </div>
+            </nav>
+        </header>
+    )
+}
